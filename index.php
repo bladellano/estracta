49b62102a85c724579417860f647638e9f792eb1
bladellano <bladellano@gmail.com>
2020-03-09 22:35:14 -0300

    Subindo arquivos

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>eStracta</title>
+</head>
+<body>
+    <?php
+    
+    require("./vendor/autoload.php");
+    
+    use Goutte\Client as GoutteClient;
+    use GuzzleHttp\Client as GuzzleClient;
+
+    $guzzleClient = new GuzzleClient(array(
+        'timeout' => 60,
+    ));    
+    $goutteClient = new GoutteClient();    
+    $goutteClient->setClient($guzzleClient);
+
+    $crawler = $goutteClient->request('GET', 'http://www.guiatrabalhista.com.br/guia/salario_minimo.htm');
+
+    $table = $crawler->filter('table[width=1050]')->filter('tr')->each(function ($tr, $i) {          
+        return $tr->filter('td')->each(function ($td, $i) {
+            return trim($td->text());
+        });
+    });
+    array_shift($table);
+
+    echo json_encode($table); 
+
+    ?>
+</body>
+</html>
